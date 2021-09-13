da5f5e3c40f555b1a8edf573616444b52effadc2
Yusuf Çaycı <yusufcayci@Yusuf-MacBook-Air.local>
2021-09-13 13:11:49 +0300

    db configurations was set

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
 const winston = require('winston');
+const config = require('config');
 
 module.exports = function () {
 
-    mongoose.connect('mongodb://localhost/pandemiceo')
-        .then(() => winston.info('Connected to MongoDB...'));
+    const db = config.get('db');
+    mongoose.connect(db)
+        .then(() => winston.info(`Connected to ${db}...`));
 }
